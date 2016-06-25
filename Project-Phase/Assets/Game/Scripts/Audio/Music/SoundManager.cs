0d5f8a7405c6ea3ff998fc1228a144fe29cb4bfe
Matthew Chambers <Matthew Chambers>
2016-06-25 21:24:01 +0100

    Adding Sound Manager

---
+++ Project-Phase/Assets/Game/Scripts/Audio/Music/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public float volume = 1.0f;
+
+	AudioSource audioSource;
+	public AudioClip[] audioClips;
+
+	void OnEnable() {
+		audioSource = this.gameObject.GetComponent<AudioSource>();
+		Events.onTabletPickedUpEvent += SetMusicPlay;
+	}
+	
+	void OnDisable() {
+		Events.onTabletPickedUpEvent -= SetMusicPlay;
+	}
+
+	void SetMusicPlay(bool toggle = false) {
+		if (toggle) {
+			audioSource.Play();
+		} else {
+			audioSource.Pause();
+		}
+	}
+
+	void Update(){
+		audioSource.volume = volume;
+		if (audioClips.Length != 0){
+			if (!audioSource.isPlaying) {
+				playRandomTrack ();
+			}
+		}
+	}
+	
+	public void playRandomTrack (){
+		int randomIndex = Random.Range (0, audioClips.Length);
+		audioSource.clip = audioClips [randomIndex];
+		audioSource.Play ();
+	}
+}
