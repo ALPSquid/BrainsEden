a3e568542ea9b813faec5d600c7317e919de569e
ALPSquid <BrainsEden37@COM217-4-1.ANGLIA.LOCAL>
2016-06-25 11:40:11 +0100

    Adds base player input

---
+++ Project-Phase/Assets/Game/Scripts/Input/InputMappings.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Handles mapping between game-friendly actions and Unity InputManager
+/// </summary>
+public static class InputMappings {
+
+    public enum EAction {
+        MOVE_FORWARD, MOVE_RIGHT, JUMP, PUSH, PULL, SWITCH_PHASE
+    }
+
+    private static Dictionary<EAction, string> InputNames = new Dictionary<EAction,string> {
+        {EAction.MOVE_FORWARD, "Move Forward"},
+        {EAction.MOVE_RIGHT, "Move Right"},
+        {EAction.PUSH, "Push"},
+        {EAction.PULL, "Pull"},
+        {EAction.SWITCH_PHASE, "Switch Phase"}
+    };
+
+    /*private static List<EAction> Axes = new List<EAction> {
+        EAction.MOVE_FORWARD,
+        EAction.MOVE_RIGHT,
+    };*/
+
+
+    /// <summary>
+    /// Returns physical input for a specified action
+    /// </summary>
+    /// <param name="action">Action to query input for</param>
+    /// <returns>Float Axis value, or 0 or 1 for buttons</returns>
+    public static bool GetInput(EAction action) {
+        //if (Axes.Contains(action)) return Input.GetAxis(InputNames[action]);
+        //return Input.GetButton(InputNames[action])? 1 : 0;
+        return Input.GetButton(InputNames[action]);
+    }
+	
+}
--- Project-Phase/Assets/Game/Scripts/Player/PlayerController.cs
+++ Project-Phase/Assets/Game/Scripts/Player/PlayerController.cs
@@ -1,11 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityStandardAssets.Characters.FirstPerson;
 
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class PlayerController : MonoBehaviour {
 
+    [Header("Movement")]
+    public float movementSpeed = 40f;
+    public float maxSpeed = 15f;
+
+    [Header("Controls")]
     public MouseLook mouseLook = new MouseLook();
+
     private GameManager gameManager;
+    // Cache of input values
+    private Dictionary<InputMappings.EAction, bool> InputValues;
+    // Input Values last frame
+    private Dictionary<InputMappings.EAction, bool> LastInputValues;
+
+    private CapsuleCollider collider;
+    private Rigidbody body;
 
 
     void Start() {
@@ -20,10 +37,27 @@ public class PlayerController : MonoBehaviour {
     }
 
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Q)) {
-			
-		}
         mouseLook.LookRotation(transform, Camera.main.transform);
+        HandleInput();
 
+        LastInputValues = InputValues;
 	}
+
+    void FixedUpdate() {
+        if (InputValues[InputMappings.EAction.MOVE_FORWARD]) {
+            body.AddForce(body.transform.forward * movementSpeed, ForceMode.Acceleration);
+        }
+    }
+
+    /// <summary>
+    /// Gets input and stores it in InputValues and handles non-physics input
+    /// </summary>
+    private void HandleInput() {
+        InputValues[InputMappings.EAction.MOVE_FORWARD] = InputMappings.GetInput(InputMappings.EAction.MOVE_FORWARD);
+        InputValues[InputMappings.EAction.MOVE_RIGHT] = InputMappings.GetInput(InputMappings.EAction.MOVE_RIGHT);
+        InputValues[InputMappings.EAction.JUMP] = InputMappings.GetInput(InputMappings.EAction.JUMP);
+        InputValues[InputMappings.EAction.SWITCH_PHASE] = InputMappings.GetInput(InputMappings.EAction.SWITCH_PHASE);
+        InputValues[InputMappings.EAction.PUSH] = InputMappings.GetInput(InputMappings.EAction.PUSH);
+        InputValues[InputMappings.EAction.PULL] = InputMappings.GetInput(InputMappings.EAction.PULL); 
+    }
 }
