bd5297ea5df8c4932922680cd2a516a9dfe841af
ALPSquid <Administrator@COM217-4-1.ANGLIA.LOCAL>
2016-06-26 09:52:20 +0100

    Adds holding functionality to button mappings

--- Project-Phase/Assets/Game/Scripts/Input/InputMappings.cs
+++ Project-Phase/Assets/Game/Scripts/Input/InputMappings.cs
@@ -11,7 +11,7 @@ public static class InputMappings {
         MOVE_FORWARD, MOVE_RIGHT, JUMP, PUSH, PULL, SWITCH_PHASE, INTERACT
     }
 
-    private static Dictionary<EAction, string> InputNames = new Dictionary<EAction,string> {
+    private static Dictionary<EAction, string> inputNames = new Dictionary<EAction,string> {
         {EAction.MOVE_FORWARD, "Move Forward"},
         {EAction.MOVE_RIGHT, "Move Right"},
         {EAction.JUMP, "Jump"},
@@ -26,9 +26,18 @@ public static class InputMappings {
         EAction.MOVE_RIGHT,
     };
 
-    private static Dictionary<EAction, float> AxisDeadzones = new Dictionary<EAction, float>() {
+    private static Dictionary<EAction, float> axisDeadzones = new Dictionary<EAction, float>() {
         {EAction.MOVE_FORWARD, 0},
         {EAction.MOVE_RIGHT, 0}
+    };
+
+    // Whether the button can be held down or must be released between triggers
+    private static Dictionary<EAction, bool> canBeHeld = new Dictionary<EAction, bool>() {
+        {EAction.JUMP, false},
+        {EAction.PUSH, true},
+        {EAction.PULL, true},
+        {EAction.SWITCH_PHASE, false},
+        {EAction.INTERACT, false}
     };
 
     /// <summary>
@@ -38,9 +47,12 @@ public static class InputMappings {
     /// <returns>Float Axis value, or 0 or 1 for buttons</returns>
     public static float GetInput(EAction action) {
         if (Axes.Contains(action)) {
-            float input =  Input.GetAxis(InputNames[action]);
-            return (Mathf.Abs(input) > AxisDeadzones[action]) ? input : 0;
+            float input =  Input.GetAxis(inputNames[action]);
+            return (Mathf.Abs(input) > axisDeadzones[action]) ? input : 0;
         }
-        return Input.GetButtonDown(InputNames[action])? 1 : 0;
+        if (canBeHeld[action]) {
+            return Input.GetButton(inputNames[action]) ? 1 : 0;
+        }
+        return Input.GetButtonDown(inputNames[action])? 1 : 0;
     }	
 }
